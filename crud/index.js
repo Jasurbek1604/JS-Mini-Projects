f9d9a43ce5f02863235552c49dc67c12e3de83b7
jasurbek <jasurdev1604@gmail.com>
2023-11-06 01:31:53 +0500

    CRUD

--- crud/index.js
+++ crud/index.js
@@ -46,4 +46,46 @@ let data = [
   },
 ];
 
+function* generatorFunc() {
+  let id = data.length + 1;
+  while (1) {
+    yield ++id;
+  }
+}
+
+let gen = generatorFunc();
+
+const onDelete = (id) => {
+  data = data.filter((e) => e.id !== id);
+};
+
+const addItem = (first, last, email, phone, location, hobby) => {
+  data = [
+    ...data,
+    {
+      id: gen.next().value,
+      first,
+      last,
+      email,
+      phone,
+      location,
+      hobby,
+    },
+  ];
+};
+
+// addItem("none", "none", "none", "none", "none", "none");
+
+const update = (none) => {
+  data = data.map((item) => {
+    if (item.id === none.id) {
+      return { ...item, [none.type]: none.value };
+    } else {
+      return item;
+    }
+  });
+};
+
+update({ id: 3, type: "hobby", value: "none" });
+
 console.log(data);
