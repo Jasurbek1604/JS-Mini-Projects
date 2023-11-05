61c0898bf5068b412ee10173f3a20f4246f8685f
jasurbek <jasurdev1604@gmail.com>
2023-11-05 23:16:16 +0500

    CRUD

---
+++ crud/index.js
@@ -0,0 +1,49 @@
+let data = [
+  {
+    id: 1,
+    first: "Jasurbek",
+    last: "Otelbayev",
+    email: "jasurdev1604@gmail.com",
+    phone: "+998935960246",
+    location: "Tashkent",
+    hobby: "Coding",
+  },
+  {
+    id: 2,
+    first: "Jasurbek",
+    last: "Otelbayev",
+    email: "jasurdev1604@gmail.com",
+    phone: "+998935960246",
+    location: "Tashkent",
+    hobby: "Coding",
+  },
+  {
+    id: 3,
+    first: "Jasurbek",
+    last: "Otelbayev",
+    email: "jasurdev1604@gmail.com",
+    phone: "+998935960246",
+    location: "Tashkent",
+    hobby: "Coding",
+  },
+  {
+    id: 4,
+    first: "Jasurbek",
+    last: "Otelbayev",
+    email: "jasurdev1604@gmail.com",
+    phone: "+998935960246",
+    location: "Tashkent",
+    hobby: "Coding",
+  },
+  {
+    id: 5,
+    first: "Jasurbek",
+    last: "Otelbayev",
+    email: "jasurdev1604@gmail.com",
+    phone: "+998935960246",
+    location: "Tashkent",
+    hobby: "Coding",
+  },
+];
+
+console.log(data);
