c5fc71f50b09101b24e209f6d7a2a8f4e9d01184
jasurbek <jasurdev1604@gmail.com>
2023-11-06 18:07:32 +0500

    CRUD

--- crud/index.js
+++ crud/index.js
@@ -46,46 +46,117 @@ let data = [
   },
 ];
 
-function* generatorFunc() {
-  let id = data.length + 1;
+function* g() {
+  i = data.length;
   while (1) {
-    yield ++id;
+    yield ++i;
   }
 }
+let gen = g();
 
-let gen = generatorFunc();
-
-const onDelete = (id) => {
-  data = data.filter((e) => e.id !== id);
-};
-
-const addItem = (first, last, email, phone, location, hobby) => {
-  data = [
-    ...data,
-    {
-      id: gen.next().value,
-      first,
-      last,
-      email,
-      phone,
-      location,
-      hobby,
-    },
-  ];
-};
-
-// addItem("none", "none", "none", "none", "none", "none");
-
-const update = (none) => {
-  data = data.map((item) => {
-    if (item.id === none.id) {
-      return { ...item, [none.type]: none.value };
-    } else {
-      return item;
-    }
+const getData = (data) => {
+  data.map((item) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${item.id}</td>
+    <td>${item.first}</td>
+    <td>${item.last}</td>
+    <td>${item.email}</td>
+    <td>${item.phone}</td>
+    <td>${item.location}</td>
+    <td>${item.hobby}</td>
+    <td>
+      <button onclick = "edit(${item.id})" class="btn bg-warning text-light">Edit</button>
+      <button onclick="onDel(${item.id})" class="btn bg-danger text-light">Delete</button>
+    </td>
+      `;
+    tbody.appendChild(tr);
   });
 };
 
-update({ id: 3, type: "hobby", value: "none" });
+getData(data);
 
-console.log(data);
+function onDel(id) {
+  tbody.innerHTML = null;
+  data = data.filter((e) => e.id !== id);
+  getData(data);
+}
+
+function add() {
+  if (
+    first.value !== "" &&
+    last.value !== "" &&
+    email.value !== "" &&
+    phone.value !== "" &&
+    loc.value !== "" &&
+    hobby.value !== ""
+  ) {
+    data = [
+      ...data,
+      {
+        id: gen.next().value,
+        first: first.value,
+        last: last.value,
+        email: email.value,
+        phone: phone.value,
+        location: loc.value,
+        hobby: hobby.value,
+      },
+    ];
+    tbody.innerHTML = "";
+    getData(data);
+    first.value = "";
+    last.value = "";
+    email.value = "";
+    phone.value = "";
+    loc.value = "";
+    hobby.value = "";
+  } else {
+    alert("no input");
+  }
+}
+
+function edit(id) {
+  tbody.innerHTML = null;
+  data = data.map((item) => {
+    let tr = document.createElement("tr");
+    if (item.id === id) {
+      tr.innerHTML = `
+        <td>${id}</td>
+        <td><input class="form-control" value="${item.first}" type="text"/></td>
+        <td><input class="form-control" value="${item.last}" type="text"/></td>
+        <td><input class="form-control" value="${item.email}" type="text"/></td>
+        <td><input class="form-control" value="${item.phone}" type="text"/></td>
+        <td><input class="form-control" value="${item.location}" type="text"/></td>
+        <td><input class="form-control" value="${item.hobby}" type="text"/></td>
+        <td>
+          <button onclick="save()" class="btn bg-success text-light">Save</button>
+          <button onclick="cancle()" class="btn bg-warning text-light">Cancle</button>
+        </td>
+      `;
+    } else {
+      tr.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.first}</td>
+        <td>${item.last}</td>
+        <td>${item.email}</td>
+        <td>${item.phone}</td>
+        <td>${item.location}</td>
+        <td>${item.hobby}</td>  
+        <td>
+          <button onclick = "edit(${item.id})" class="btn bg-warning text-light">Edit</button>
+          <button onclick="onDel(${item.id})" class="btn bg-danger text-light">Delete</button>
+        </td>
+      `;
+    }
+    tbody.appendChild(tr);
+    return item;
+  });
+}
+
+function cancle() {
+  tbody.innerHTML = "";
+  getData(data);
+}
+
+function save() {}
