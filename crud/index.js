027a7bed1db9c0bb3fc90bc0cdf6c87d49cf69e7
jasurbek <jasurdev1604@gmail.com>
2023-11-07 13:47:09 +0500

    CRUD

--- crud/index.js
+++ crud/index.js
@@ -4,45 +4,30 @@ let data = [
     first: "Jasurbek",
     last: "Otelbayev",
     email: "j@gmail.com",
-    phone: "+998935960246",
-    location: "Tashkent",
-    hobby: "Coding",
   },
   {
     id: 2,
     first: "Jasurbek",
     last: "Otelbayev",
     email: "j@gmail.com",
-    phone: "+998935960246",
-    location: "Tashkent",
-    hobby: "Coding",
   },
   {
     id: 3,
     first: "Jasurbek",
     last: "Otelbayev",
     email: "j@gmail.com",
-    phone: "+998935960246",
-    location: "Tashkent",
-    hobby: "Coding",
   },
   {
     id: 4,
     first: "Jasurbek",
     last: "Otelbayev",
     email: "j@gmail.com",
-    phone: "+998935960246",
-    location: "Tashkent",
-    hobby: "Coding",
   },
   {
     id: 5,
     first: "Jasurbek",
     last: "Otelbayev",
     email: "j@gmail.com",
-    phone: "+998935960246",
-    location: "Tashkent",
-    hobby: "Coding",
   },
 ];
 
@@ -62,9 +47,6 @@ const getData = (data) => {
     <td>${item.first}</td>
     <td>${item.last}</td>
     <td>${item.email}</td>
-    <td>${item.phone}</td>
-    <td>${item.location}</td>
-    <td>${item.hobby}</td>
     <td>
       <button onclick = "edit(${item.id})" class="btn bg-warning text-light">Edit</button>
       <button onclick="onDel(${item.id})" class="btn bg-danger text-light">Delete</button>
@@ -83,14 +65,7 @@ function onDel(id) {
 }
 
 function add() {
-  if (
-    first.value !== "" &&
-    last.value !== "" &&
-    email.value !== "" &&
-    phone.value !== "" &&
-    loc.value !== "" &&
-    hobby.value !== ""
-  ) {
+  if (first.value !== "" && last.value !== "" && email.value !== "") {
     data = [
       ...data,
       {
@@ -98,9 +73,6 @@ function add() {
         first: first.value,
         last: last.value,
         email: email.value,
-        phone: phone.value,
-        location: loc.value,
-        hobby: hobby.value,
       },
     ];
     tbody.innerHTML = "";
@@ -108,9 +80,6 @@ function add() {
     first.value = "";
     last.value = "";
     email.value = "";
-    phone.value = "";
-    loc.value = "";
-    hobby.value = "";
   } else {
     alert("no input");
   }
@@ -126,9 +95,6 @@ function edit(id) {
         <td><input id="nn" class="form-control" value="${item.first}" type="text"/></td>
         <td><input id="ss" class="form-control" value="${item.last}" type="text"/></td>
         <td><input id="ee" class="form-control" value="${item.email}" type="text"/></td>
-        <td><input id="pp" class="form-control" value="${item.phone}" type="text"/></td>
-        <td><input id="ll" class="form-control" value="${item.location}" type="text"/></td>
-        <td><input id="hh" class="form-control" value="${item.hobby}" type="text"/></td>
         <td class="d-flex gap-1">
           <button onclick="save(${id})" class="btn bg-success text-light">Save</button>
           <button onclick="cancle()" class="btn bg-warning text-light">Cancle</button>
@@ -140,9 +106,6 @@ function edit(id) {
         <td>${item.first}</td>
         <td>${item.last}</td>
         <td>${item.email}</td>
-        <td>${item.phone}</td>
-        <td>${item.location}</td>
-        <td>${item.hobby}</td>  
         <td>
           <button onclick = "edit(${item.id})" class="btn bg-warning text-light">Edit</button>
           <button onclick="onDel(${item.id})" class="btn bg-danger text-light">Delete</button>
@@ -167,9 +130,6 @@ function save(id) {
         first: nn.value,
         last: ss.value,
         email: ee.value,
-        phone: pp.value,
-        location: ll.value,
-        hobby: hh.value,
       };
     } else {
       return item;
